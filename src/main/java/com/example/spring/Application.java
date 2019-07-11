45d8a9f70cb6f988364996ebf9b05f2d49debd08
Lyaman98 <lyamanava@gmail.com>
2019-07-11 13:11:53 +0400

    committing changes

--- src/main/java/com/example/spring/Application.java
+++ src/main/java/com/example/spring/Application.java
@@ -1,13 +1,24 @@
 package com.example.spring;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+    @Value("${application.message}")
+    private String applicationMessage;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @GetMapping("/message")
+    public String getMessage(){
+        return "Message : " + applicationMessage;
+    }
 }
